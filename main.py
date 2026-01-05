4bae86e96352a8bfa967991610e2c1a9bf77f220
xfac11 <xfac115@gmail.com>
2026-01-05 14:42:04 +0100

    Added delta time variable and set the fps to 60

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
@@ -15,6 +17,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+
+
+        dt = clock.tick(60) / 1000
     
 
 if __name__ == "__main__":
