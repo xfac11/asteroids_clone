39c304a3aa8af031bedb3005e5cb3865786cb5ed
xfac11 <xfac115@gmail.com>
2026-01-05 17:35:43 +0100

    Added collision with shots and asteroid. Currently the asteroids dissapear when a shot is collided with them. Also the shot dissapears. Dissapear by using kill() from pygames Sprite class

--- main.py
+++ main.py
@@ -42,6 +42,12 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
         for obj in drawable:
             obj.draw(screen)
 
